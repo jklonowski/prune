86631f7968c8b31b5448e8f24cfae7953b0a917f
Joe Klonowski <jklonowski@acvauctions.com>
2018-11-07 15:40:16 -0500

    First commit

---
+++ batch_process.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Transport - Batch Fixer</title>
+</head>
+
+<body>
+<?php
+if ( isset($_POST["Submit"]) ) 
+{
+	//Take address swap CSV and throw it into an array
+	if (($csvfile = fopen("add_swap.csv", "r")) !== FALSE) 
+	{
+    	while (($data = fgetcsv($csvfile, 1000, ",")) !== FALSE) 
+		{
+			$add_csv[] = $data;
+    	}
+    	fclose($csvfile);
+	}
+	print_r($add_csv);
+	//handle upload and setup batch file into an array
+	$file = $_FILES["file"]['tmp_name']; 
+	$handle = fopen($file,"r"); 
+	
+	while (($row = fgetcsv($handle, 1200, ",")) !== FALSE) 
+	{
+		if (is_numeric($row['0']))		
+		{
+			$auctionid = $row['0'];
+			$p_id = $row['1'];
+			$p_name = $row['2'];
+			$p_dba = $row['3'];
+			$p_address = $row['4'];
+			$p_city = $row['5'];
+			$p_state = $row['6'];
+			$p_zip = $row['7'];
+			$p_contact = $row['8'];
+			$p_phone = $row['9'];
+			$d_name = $row['10'];
+			$d_id = $row['11'];
+			$d_dba = $row['12'];
+			$d_address = $row['13'];
+			$d_city = $row['14'];
+			$d_state = $row['15'];
+			$d_zip = $row['16'];
+			$d_contact = $row['17'];
+			$d_phone = $row['18'];
+			$charged = $row['19'];
+			$vin = $row['20'];
+			$year = $row['21'];
+			$make = $row['22'];
+			$model = $row['23'];
+			$trim = $row['24'];
+			$type = $row['25'];
+			$green = $row['26'];
+			$pay_status = $row['27'];
+			$release = $row['28'];
+			$flag1 = $row['29'];   //condition report fields
+			$flag2 = $row['30'];
+			$flag3 = $row['31'];
+			$flag4 = $row['32'];
+			$flag5 = $row['33'];
+		}
+	}	
+	
+
+echo $str;
+}
+?>   
+</body>
+</html>
---
+++ batch_upload.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Transport - Upoload to the The Batchery</title>
+<style type="text/css">
+<!--
+.style1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 24px;
+}
+.style2 {
+	font-size: 14px;
+	font-family: Arial, Helvetica, sans-serif;
+}
+.style3 {
+	font-size: 14px;
+	font-style: italic;
+}
+-->
+</style>
+</head>
+
+<body>
+
+
+<div align="center">
+  <p>&nbsp;</p>
+  <p class="style1">The Batchery - Upload<br />
+  <span class="style3">Pruning lists since 2018  </span></p>
+  <p class="style1"><img src="prune.jpg" alt="Matt smells like 3 week old dog fart!" width="225" height="225" /></p>
+  <p>&nbsp;</p>
+  <form id="form1" name="form1" enctype="multipart/form-data" method="post" action="batch_process.php">
+    <label><span class="style2">CSV File</span>
+      <input type="file" name="file" />
+    </label>
+    <p>&nbsp;</p>
+    <p>
+      <label>
+      <input type="submit" name="Submit" value="Upload" />
+      </label>
+    </p>
+  </form>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</div>
+</body>
+</html>
