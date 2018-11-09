846c01c6af54d7df6ba7c6c14753a192a4ba8c7a
jklonowski <jklonowski@acvauctions.com>
2018-11-08 23:57:17 -0500

    Ready List started

--- batch_process.php
+++ batch_process.php
@@ -5,16 +5,20 @@
 <title>Transport - Batch Fixer</title>
 <style type="text/css">
 <!--
-.style2 {font-size: 24px}
+.style8 {font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 14px; color: #FFFFFF; }
+.style11 {font-family: Arial, Helvetica, sans-serif; font-size: 14px; color: #000000; }
+.style12 {font-size: 12px}
+.style13 {font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 12px; color: #FFFFFF; }
+.style14 {font-family: Arial, Helvetica, sans-serif}
+.style15 {font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
+.style16 {font-family: Arial, Helvetica, sans-serif; font-weight: bold; }
 -->
 </style>
 </head>
 
 <body>
-<p>
-  <span class="style2">You've been Shuecklered! </span><br />
-  <br />
-  <?php
+<?php
+
 if ( isset($_POST["Submit"]) ) 
 {
 	//Take address swap CSV and throw it into an array
@@ -36,27 +40,10 @@ if ( isset($_POST["Submit"]) )
 	{
 		if (is_numeric($row['0']))		
 		{
-			$auctionid = $row['0'];
-			$p_id = $row['1'];
-			$p_name = $row['2'];
-			$p_dba = $row['3'];
-			$p_address = $row['4'];
-			$p_city = $row['5'];
-			$p_state = $row['6'];
-			$p_zip = $row['7'];
-			$p_contact = $row['8'];
-			$p_phone = $row['9'];
-			$d_name = $row['10'];
-			$d_id = $row['11'];
-			$d_dba = $row['12'];
-			$d_address = $row['13'];
-			$d_city = $row['14'];
-			$d_state = $row['15'];
-			$d_zip = $row['16'];
-			$d_contact = $row['17'];
-			$d_phone = $row['18'];
+			if ($row['3'] == "") { $row['3'] = $row['2']; }
+			if ($row['12'] == "") { $row['12'] = $row['11']; }
 
-			
+			/*
 			if (isset($add_csv[$p_id]) && $add_csv[$p_id]['6'])
 			{
 				$row['4'] = $add_csv[$p_id]['2'];
@@ -73,9 +60,23 @@ if ( isset($_POST["Submit"]) )
 				$row['16'] = $add_csv[$d_id]['5'];
 				echo "Swapped out $d_id - $d_name - $d_dba </br>";
 			}
+			*/
+		
+
+			if ($row['28'] == "1" && $row['3'] <> "ACV"){
+			//If there is a pickup Slip available
+				if ($row['29'] == "1" || $row['29'] == "1" || $row['30'] == "1" || $row['31'] == "1" || $row['32'] == "1" || $row['33'] == "1") {
+				//If there are any in-op flags in the condition report fields
+					$inop[] = $row;
+				} else {
+				//If no inop fields flagged
+					$ready[] = $row;
+				}
+			}
 		}
-		$new_csv[] = $row;
 	}	
+			print_r($row);
+	/*
 	unlink('ACV_pruned.csv');
 	$fp = fopen('ACV_pruned.csv', 'w');
 	foreach ($new_csv as $value)
@@ -83,11 +84,97 @@ if ( isset($_POST["Submit"]) )
 		fputcsv($fp, $value);
 	}
 fclose($fp);
-//print_r($new_csv);
+*/
+		
 }
-?>   
-</p>
+
+?>
+<br />
+<br />
+<form id="form1" name="form1" method="post" action="batch_pruned.php">
+  <table width="1400" border="0" align="center" cellpadding="3" cellspacing="1">
+    <tr>
+      <td colspan="13"><div align="center"><span class="style16">Ready to Rock </span></div></td>
+    </tr>
+    <tr>
+      <td colspan="2"><div align="center" class="style11">
+        </div></td>
+      <td colspan="7"><div align="center" class="style16"></div></td>
+      <td colspan="4"><div align="right" class="style14">
+        <div align="center"><?php printf(count($ready)); ?> Ready Jobs </div>
+      </div></td>
+    </tr>
+    <tr>
+      <td bgcolor="#000000" class="style8"><div align="center"></div></td>
+      <td colspan="5" bgcolor="#000000" class="style8"><div align="center">Pickup Info </div></td>
+      <td colspan="5" bgcolor="#000000" class="style8"><div align="center">Delivery Info </div></td>
+      <td colspan="2" bgcolor="#000000" class="style8"><div align="center">Monitary</div></td>
+    </tr>
+    <tr>
+      <td bgcolor="#330000"><div align="center" class="style8 style12 style14">Auction ID </div></td>
+      <td bgcolor="#000033"><div align="center" class="style13">Name</div></td>
+      <td bgcolor="#000033"><div align="center" class="style13">Address</div></td>
+      <td bgcolor="#000033"><div align="center" class="style13">City</div></td>
+      <td bgcolor="#000033"><div align="center" class="style13">State</div></td>
+      <td bgcolor="#000033"><div align="center" class="style13">Zip</div></td>
+      <td bgcolor="330033"><div align="center" class="style13">Name</div></td>
+      <td bgcolor="330033"><div align="center" class="style13">Address</div></td>
+      <td bgcolor="330033"><div align="center" class="style13">City</div></td>
+      <td bgcolor="330033"><div align="center" class="style13">State</div></td>
+      <td bgcolor="330033"><div align="center" class="style13">Zip</div></td>
+      <td bgcolor="#003300"><div align="center" class="style13">Charged</div></td>
+      <td width="80" bgcolor="#003300"><div align="center" class="style13">Payout</div></td>
+    </tr>
+    <?php if(isset($ready))
+  { 
+  	$bcolor  = "#F1EEEE";  //starting row background color
+  	foreach($ready as $job) {
+	
+	if ($bcolor == "#FFFFFF")   //alternate row background colors
+  	{ 
+		$bcolor = "#F1EEEE"; 
+  	} else { 
+		$bcolor = "#FFFFFF"; 
+  	}
+	 ?>
+    <tr bgcolor="<?php echo $bcolor; ?>" onMouseOver="this.bgColor = '#C2F3C6'" onMouseOut ="this.bgColor = '<?php echo $bcolor ?>'">
+      <td><span class="style15"><?php printf($job['0']); ?></span></td>
+      <td><span class="style15"><?php printf($job['3']); ?></span></td>
+      <td><span class="style15"><?php printf($job['4']); ?></span></td>
+      <td><span class="style15"><?php printf($job['5']); ?></span></td>
+      <td><span class="style15"><?php printf($job['6']); ?></span></td>
+      <td><span class="style15"><?php printf($job['7']); ?></span></td>
+      <td><span class="style15"><?php printf($job['12']); ?></span></td>
+      <td><span class="style15"><?php printf($job['13']); ?></span></td>
+      <td><span class="style15"><?php printf($job['14']); ?></span></td>
+      <td><span class="style15"><?php printf($job['15']); ?></span></td>
+      <td><span class="style15"><?php printf($job['16']); ?></span></td>
+      <td><div align="center"><span class="style15">$<?php printf(number_format($job['19'],0)); ?></span></div></td>
+      <td width="80"><span class="style15">
+        <label> $
+          <input name="textfield" type="text" class="style15" size="5" maxlength="5" />
+        </label>
+      </span></td>
+    </tr>
+    <?php } 
+  }?>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td width="80">&nbsp;</td>
+    </tr>
+  </table>
+</form>
 <p>&nbsp;</p>
-<p><a href="ACV_pruned.csv">Pruned List </a></p>
 </body>
 </html>
